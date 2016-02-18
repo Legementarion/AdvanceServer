b96172e35b3fa9cc06687544e25a7a19142a0151
Legementario <mr.re66it@gmail.com>
2016-02-18 11:04:03 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import com.company.Connection.Connect;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Connect connect = new Connect();
+        connect.Connection();
+     }
+}
